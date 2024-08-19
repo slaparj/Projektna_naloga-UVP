71612c1d9a77e1cf35dc6916bc62886e71d3d98b
Jaka Slapar <js02630@student.uni-lj.si>
2024-08-19 12:50:35 +0200

    Shranjevanje podatkov v csv

--- shrani_v_csv.py
+++ shrani_v_csv.py
@@ -1,10 +1,18 @@
 import csv
+import os
 
 def shrani_v_csv(seznam):
+    
+    direktorij = 'csv_datoteke'
 
-    csv_dat = 'manga_info.csv'
+    if not os.path.exists(direktorij):
+        os.makedirs(direktorij)
+        
+        csv_dat = 'manga_info.csv'
+        
+        dat_ime = os.path.join(direktorij, csv_dat)
 
-    with open(csv_dat, mode='w', newline='', encoding='utf-8') as dat:
+    with open(dat_ime, mode='w', newline='', encoding='utf-8') as dat:
         writer = csv.DictWriter(dat, fieldnames=seznam[0].keys())
         writer.writeheader()
         writer.writerows(seznam)
