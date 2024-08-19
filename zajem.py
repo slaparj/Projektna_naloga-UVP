e3c776dc2ef9f1a93534d62d4ece043a3697a7d0
Jaka Slapar <js02630@student.uni-lj.si>
2024-08-19 11:23:31 +0200

    Spoj datotek 'zajem' in 'zajem2'

--- zajem.py
+++ zajem.py
@@ -19,13 +19,16 @@ def izlusci(url):
         ocena = manga.find('td', class_="score").text.strip()
         link = manga.find('a')['href']
     
-        #iščemo še podatke shranjene v dodatnem 'hover oblačku'
+    
+    
+        #najdemo še število poglavij
         podatki = manga.find('div', class_='information di-ib mt4')
         podatki_text = podatki.text.strip().replace('\n', ' ')
     
         deli_najdba = re.search(r'(\d+) vols', podatki_text)
-    
         deli = deli_najdba.group(1) if deli_najdba else 'Ni končana'
+    
+    
     
         lastnosti_mange = {
             'rank': rank,
@@ -34,11 +37,69 @@ def izlusci(url):
             'deli': deli,
         }
         
+    
+        #temu slovarju dodamo še slovar dodatnik podatkov, ki ga najde funkcija 'dodatni_podatki'
         dodatni_seznam = dodatne_lastnosti(link)
-        
         lastnosti_mange.update(dodatni_seznam)
         
         #print(f'{rank}. {naslov}, {ocena} {deli}')
         seznam_mang.append(lastnosti_mange)
         
     return seznam_mang
+
+
+
+#funkcija, ki vsaki mangi poišče dodatne lastnosti. To stori, tako da obišče link vsake mange posebaj in izlušči popularnost, seznam žanrov in revijo, ki mango objavlja
+def dodatne_lastnosti(url_mange):
+    
+    html_mange = requests.get(url_mange)
+    juha_mange = BeautifulSoup(html_mange.content, 'lxml')
+    
+    
+    
+    #Poiščemo kako popularna je manga
+    popularnost_najdba = juha_mange.find('span', class_='numbers popularity')
+    popularnost = int(''.join(re.findall(r'\d+', popularnost_najdba.text.strip()))) if popularnost_najdba else 'Nismo našli popularnosti'
+    
+    
+    
+    #Poiščemo v katere žanre pripada manga
+    seznam_zanrov = []
+    
+    if not juha_mange.find('span', string='Genres:') and not juha_mange.find('span', string='Genre:'):
+        seznam_zanrov = ['Nismo našli informacij']
+    else:
+        if juha_mange.find('span', string='Genres:'):
+            zanri_naslov = juha_mange.find('span', string='Genres:')
+
+        else:
+            zanri_naslov = juha_mange.find('span', string='Genre:')
+        bratec = zanri_naslov.next_sibling
+        while bratec:
+            seznam_zanrov.append(bratec.text.strip())
+            bratec = bratec.next_sibling
+        
+    seznam_zanrov = list(dict.fromkeys(seznam_zanrov))    
+    if '' in seznam_zanrov:
+        seznam_zanrov.remove('') 
+    if ',' in seznam_zanrov:  
+        seznam_zanrov.remove(',') 
+    
+    
+    
+    #Najdimo še, v kateri reviji je manga objavljena
+    revija_najdba = juha_mange.find('span', class_='information season')
+    revija = revija_najdba.a.text.strip() if revija_najdba else 'Nismo našli revije'
+    
+    
+    
+    #Pridobljene podatke shranimo v slovar
+    dodatni_podatki = {
+        'popularnost': popularnost,
+        'seznam_zanrov': seznam_zanrov,
+        'revija': revija
+    }
+
+    #print(f'{popularnost}, {seznam_zanrov}, {revija}')
+    
+    return dodatni_podatki
