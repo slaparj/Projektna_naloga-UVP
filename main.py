406454d69a0425963b0f82a0f0ede1fd9563375e
Jaka Slapar <js02630@student.uni-lj.si>
2024-08-24 19:57:11 +0200

    Popravek metode shranja, kot poskus razrešitve problema z pridobivanjem podatkov

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import zajem
-from shrani_v_csv import shrani_v_csv
+from shrani_v_csv import shrani
+import time
 
 naj_mange = []
 
 
 #spletna stran prikaže po 50 zaporednih mang, na vsaki spletni strani poženemo funkcijo izlusci()  
-for i in range(1):
+for i in range():
     if i == 0:
         url = 'https://myanimelist.net/topmanga.php'
     else: 
         url = f'https://myanimelist.net/topmanga.php?limit={i * 50}'
-    
+        
     naj_mange.extend(zajem.izlusci(url))
+    time.sleep(1)
 
-shrani_v_csv(naj_mange)
+print(naj_mange)
+shrani(naj_mange)
     
