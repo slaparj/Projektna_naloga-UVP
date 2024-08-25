48bfbe17c89a0aeee9e9cad258f21b05369e8dc6
Jaka Slapar <js02630@student.uni-lj.si>
2024-08-25 20:03:10 +0200

    Ustvaril sem main2 datoteko, ki posebaj zažene funkcijo dodatno v datoteki zajem in main popravil, da pobira le osnovne podatke.

--- main2.py
+++ main2.py
@@ -1,17 +1,17 @@
 import zajem
 from shrani_v_csv import shrani
 
-naj_mange = []
+naj_nekaj = []
 
 
 #spletna stran prikaže po 50 zaporednih mang, na vsaki spletni strani poženemo funkcijo izlusci()  
-for i in range(40):
+for i in range(2):
     if i == 0:
         url = 'https://myanimelist.net/topmanga.php'
     else: 
         url = f'https://myanimelist.net/topmanga.php?limit={i * 50}'
         
-    naj_mange.extend(zajem.izlusci(url))
+    naj_nekaj.extend(zajem.dodatno(url))
 
-print(naj_mange)
-shrani(naj_mange)
+print(naj_nekaj)
+shrani(naj_nekaj)
